0597181c1be48af3ad03118c1a03233a8bf0f708
Joshua-Lubin <jlmortonr@gmail.com>
2022-10-23 12:28:17 -0700

    Added GET and POST templates.

---
+++ server/src/main/java/com/cse360group19/server/handlers/GetTemplateHandler.java
@@ -0,0 +1,42 @@
+package com.cse360group19.server.handlers;
+
+import com.cse360group19.server.Utility;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import org.json.simple.JSONObject;
+
+public class GetTemplateHandler implements HttpHandler {
+
+    static final int OK = 200;
+    static final int INTERNAL_SERVER_ERROR = 500;
+
+    public void handle(HttpExchange exchange) throws IOException {
+        try {
+            String rawQuery = exchange.getRequestURI().getRawQuery();
+            Map<String, List<String>> inputObject = Utility.parseQuery(rawQuery);
+
+            HashMap<String, Object> outputObject = new HashMap<String, Object>();
+            outputObject.put("testOutput", inputObject.get("testInput").get(0));
+
+            String response = new JSONObject(outputObject).toJSONString();
+            exchange.sendResponseHeaders(OK, response.length());
+
+            OutputStream stream = exchange.getResponseBody();
+            stream.write(response.getBytes());
+            stream.close();
+        }
+        catch(Exception e) {
+            exchange.sendResponseHeaders(INTERNAL_SERVER_ERROR, 0);
+            OutputStream stream = exchange.getResponseBody();
+            stream.close();
+        }
+    }
+}
